ab1974737d1a235d2b07aa24e94e55e22129868f
Jorge Venegas <brangelina01@gmail.com>
2020-09-29 20:35:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(string)
   if string == string.upcase
-    elsif string != string.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
